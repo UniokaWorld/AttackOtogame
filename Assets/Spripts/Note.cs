19e3f81027d1086943bd9fcf2e979ae3b7099e8d
okada-z <okada@okada-z.dev>
2020-03-01 02:52:32 +0900

    commit

---
+++ Assets/Spripts/Note.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour
+{
+
+    void Update()
+    {
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+        pos.z -= 10 * Time.deltaTime;
+        myTransform.position = pos;
+        if (this.transform.position.z < -2.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Spripts/move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	bool Now =false;
+
+	void Update()
+	{
+		if (!Now)
+		{
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				Vector3 pos = transform.position;
+				if (pos.x >= -2) StartCoroutine(Move("A"));
+			}
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				Vector3 pos = transform.position;
+				if(pos.x <= 2) StartCoroutine(Move("D"));
+			}
+		}
+		
+	}
+
+	private IEnumerator Move(string key)
+	{
+		Now = true;
+		Vector3 pos = transform.position;
+		if (key == "A")
+		{
+			transform.Rotate(new Vector3(0, -90, 0));
+				for (int i=0;i <10;i++)
+				{
+					pos.x -= 0.1f;
+					transform.position = pos;
+					yield return null;
+				}
+			transform.Rotate(new Vector3(0, 90, 0));
+
+		}
+		else if (key == "D")
+		{
+			transform.Rotate(new Vector3(0, 90, 0));
+				for (int i = 0; i < 10; i++)
+				{
+					pos.x += 0.1f;
+					transform.position = pos;
+					yield return null;
+				}
+			transform.Rotate(new Vector3(0, -90, 0));
+		}
+		transform.position = pos;
+		Now = false;
+	}
+}
