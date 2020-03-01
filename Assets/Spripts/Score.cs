33b3c25c94bbfe8c36e919bd2018af8964595a0c
okada-z <okada@okada-z.dev>
2020-03-01 11:10:18 +0900

    commit

---
+++ Assets/Spripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Note")
+        {
+            Debug.Log(1234);
+            Destroy(other.gameObject);
+        }
+    }
+}
